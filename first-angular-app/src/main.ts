2064108e1c1cd42061c2f333207ec87c1b3609b3
jerrywu071 <jerrywu071@outlook.com>
2026-02-26 14:50:44 -0500

    save progress

--- first-angular-app/src/main.ts
+++ first-angular-app/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+
+import { HeaderComponent } from './app/header.component';
+
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
+
+bootstrapApplication(HeaderComponent);
